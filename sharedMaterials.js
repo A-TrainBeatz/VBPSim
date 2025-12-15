da26771b618b209d82ae466a212435c0e45f95b4
A-TrainBeatz <aydenwilhelm@gmail.com>
2025-12-15 09:43:19 -0500

    Add water material to sharedMaterials.js

--- sharedMaterials.js
+++ sharedMaterials.js
@@ -1,5 +1,5 @@
 const MATERIALS = {
   sand: { color: [210, 180, 80], density: 3 },
-  stone: { color: [120, 120, 120], density: 5 }
+  stone: { color: [120, 120, 120], density: 5 },
+  water: { color: [60, 140, 255], density: 1 }
 };
-
