0d057059baf4e384ff31d16495c4ef97c689deb4
A-TrainBeatz <aydenwilhelm@gmail.com>
2025-12-15 09:07:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`VBPSim running at http://localhost:${PORT}`);
+  console.log(`VBPSim live on port ${PORT}`);
 });
